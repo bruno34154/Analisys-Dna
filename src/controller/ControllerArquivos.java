9c3d9a1b08b2674c9230c02d178bf3b15e39f4cc
bruno34154 <www.brunomesenga@gmail.com>
2023-08-25 20:20:26 -0300

    reading text from files

---
+++ src/controller/ControllerArquivos.java
@@ -0,0 +1,14 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ControllerArquivos {
+	private File caminho;
+	public ControllerArquivos (File caminho){
+		caminho = this.caminho;
+	}
+	
+}
